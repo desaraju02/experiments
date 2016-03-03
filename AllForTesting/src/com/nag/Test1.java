b70f4eabd7de9a515b862f6b89f01b1ad2c56c27
NDESARAJ <desaraju02@gmail.com>
2016-03-03 15:06:46 -0600

    Test1 Commit after sysout change

---
+++ AllForTesting/src/com/nag/Test1.java
@@ -0,0 +1,16 @@
+package com.nag;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 6-1*0+2/2;
+		
+		//System.out.println(x);
+		
+		System.out.println("The answer is: "+x);
+
+	}
+
+}
