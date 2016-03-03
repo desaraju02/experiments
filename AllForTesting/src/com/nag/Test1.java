aab56c5477b157c823333c877c934bfa6f59fd44
NDESARAJ <desaraju02@gmail.com>
2016-03-03 15:36:21 -0600

    Added isPrime

--- AllForTesting/src/com/nag/Test1.java
+++ AllForTesting/src/com/nag/Test1.java
@@ -5,18 +5,32 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int x =7-1*0+2/2;
+		int x =16-1*0+2/2;
 		
 		//System.out.println(x);
 		
 		System.out.println("The answer is: "+x);
 		System.out.println("And isEven is "+ isEven(x));
+		System.out.println("isPrime: "+isPrime(x));
 
 	}
 	
 	public static boolean isEven(int x1)
 	{
 		return (x1%2 == 0);
+	}
+	
+	public static boolean isPrime(int x1)
+	{
+		boolean isPrime = true;
+		for(int i = 2; i < x1/2; i++)
+		{
+			if (x1%i == 0) 
+				isPrime = false;
+				break;
+		}
+		
+		return isPrime;
 	}
 
 }
