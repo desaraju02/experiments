a6e975023c3332263c49e8647791d4372d1e7a98
NDESARAJ <desaraju02@gmail.com>
2016-03-03 15:22:38 -0600

    Added another func

--- AllForTesting/src/com/nag/Test1.java
+++ AllForTesting/src/com/nag/Test1.java
@@ -5,12 +5,18 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int x = 6-1*0+2/2;
+		int x =7-1*0+2/2;
 		
 		//System.out.println(x);
 		
 		System.out.println("The answer is: "+x);
+		System.out.println("And isEven is "+ isEven(x));
 
+	}
+	
+	public static boolean isEven(int x1)
+	{
+		return (x1%2 == 0);
 	}
 
 }
